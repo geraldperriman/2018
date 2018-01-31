52b96d871a6e6a9a1d732ae3a22c6bc3058e4554
Coredelle <coredelle.smith@gmail.com>
2018-01-30 20:55:13 -0500

    add routes and posts component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { FirstComponent } from './first.component';
+import { PostsComponent } from './posts/posts.component';
 
+const ROUTES = [
+  {
+    path: '',
+    redirectTo: 'firstComponent',
+    pathMatch: 'full'
+  },
+  {
+    path: 'firstComponent',
+    component: FirstComponent,
+  },
+  {
+    path: 'posts',
+    component: PostsComponent
+  }
+
+];
 
 @NgModule({
   declarations: [
-    FirstComponent
+    FirstComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [FirstComponent]
